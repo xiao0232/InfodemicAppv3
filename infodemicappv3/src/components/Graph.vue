c09a241d1129ca7931839f9967060737be8cd3a2
hond0413 <hondsoccer0413@gmail.com>
2021-03-08 20:35:22 +0900

    file移行

---
+++ infodemicappv3/src/components/Graph.vue
@@ -0,0 +1,327 @@
+<template>
+  <div class="small">
+      <v-btn @click="test()">TEST</v-btn>
+    <h1>Data about {{topic}}</h1>
+    <div class="topicBtn">
+        <v-btn @click="pushTopicBtn('Face Mask')">Face Mask</v-btn>
+        <v-btn @click="pushTopicBtn('Vaccine')">Vaccine</v-btn>
+        <v-btn @click="pushTopicBtn('Yahoo!News')">Yahoo!News</v-btn>
+    </div>
+    <div class="dateBtn">
+        <v-btn @click="pushDateBtn('year')">year</v-btn>
+        <v-btn @click="pushDateBtn('month')">month</v-btn>
+        <!-- <v-btn @click="pushDateBtn('day')">day</v-btn> -->
+    </div>
+    <div v-if="loaded == false">
+        <v-progress-circular indeterminate color="black"></v-progress-circular>
+    </div>
+    <div v-if="loaded == true">
+        <v-container class='dateSelect'>
+            <v-row align="center">
+                <v-col cols="3">
+                    <v-select :items="yearRange" label="year" outlined v-model="selectYear"></v-select>
+                </v-col>
+                <v-col cols="3">
+                    <v-select :items="this.monthRange.map( str => parseInt(str, 10))" label="month" outlined v-if="dateState >= 2" v-model="selectMonth"></v-select>
+                </v-col>
+                <!-- <v-col cols="3">
+                    <v-select :items="dayArgs" label="day" outlined v-if="dateState == 3" v-model="selectDay"></v-select>
+                </v-col> -->
+            </v-row>
+        </v-container>
+        <line-chart v-if="loaded" :chart-data="datacollection"></line-chart>
+    </div>
+  </div>
+</template>
+
+<script>
+  import LineChart from './LineChart.js'
+
+  export default {
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+            totalMonthData: [],
+            totalDayData: [],
+            maskMonthData: [],
+            maskDayData: [],
+            vaccineMonthData: [],
+            vaccineDayData: [],
+            yahooMonthData: [],
+            yahooDayData: [],
+            topicMonthData: [],
+            topicDayData: [],
+            topic: 'Face Mask',
+            date: 'year',
+            yearRange: [2020],
+            monthRange: [],
+            dayRange: [],
+            loaded: false,                                                   //API追加時に使用
+            dateState: 1,
+            selectYear: 2020,
+            selectMonth: 2,
+            selectDay: 21,
+            datacollection: { labels: [], datasets: [] },
+            graphDataTotal: [],
+            graphDataTopic: [],
+            graphLabel: [],
+      }
+    },
+    async created(){
+        await this.axios
+        .get('https://mongo-fastapi01.herokuapp.com/api/count-dates')
+        .then((response) => {
+            this.totalDayData = response.data
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+        await this.axios
+        .get('https://mongo-fastapi01.herokuapp.com/api/count-months/')
+        .then((response) => {
+            this.totalMonthData = response.data
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+        await this.axios
+        .get('https://mongo-fastapi01.herokuapp.com/api/count-hashtags/months/マスク')
+        .then((response) => {
+            this.maskMonthData = response.data
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+        await this.axios
+        .get('https://mongo-fastapi01.herokuapp.com/api/count-hashtags/dates/マスク')
+        .then((response) => {
+            this.maskDayData = response.data
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+        await this.axios
+        .get('https://mongo-fastapi01.herokuapp.com/api/count-hashtags/months/ワクチン')
+        .then((response) => {
+            this.vaccineMonthData = response.data
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+        await this.axios
+        .get('https://mongo-fastapi01.herokuapp.com/api/count-hashtags/dates/ワクチン')
+        .then((response) => {
+            this.vaccineDayData = response.data
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+        await this.axios
+        .get('https://mongo-fastapi01.herokuapp.com/api/count-hashtags/months/Yahoo')
+        .then((response) => {
+            this.yahooMonthData = response.data
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+        await this.axios
+        .get('https://mongo-fastapi01.herokuapp.com/api/count-hashtags/dates/Yahoo')
+        .then((response) => {
+            this.yahooDayData = response.data
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+        await this.totalMonthData.filter(data => {
+            if(this.yearRange.includes(data.month.substring(0, 4)) == false){
+                this.yearRange.push(data.month.substring(0, 4))
+            }
+        })
+        await this.totalMonthData.filter(data => {
+            if(this.monthRange.includes(data.month.substring(4, 6)) == false){
+                this.monthRange.push(data.month.substring(4, 6))
+            }
+        })
+        // this.monthArgs = this.monthArgs.map( str => parseInt(str, 10))
+        this.graphLabel = this.monthRange.map( str => parseInt(str, 10))
+        this.topicMonthData = this.maskMonthData
+        this.topicDayData = this.maskDayData
+        this.loaded = true
+        this.pushTopicBtn('Face Mask')
+    },
+    mounted () {
+    },
+    computed: {
+    },
+    methods: {
+        test(){
+            var weekdays = [ "日", "月", "火", "水", "木", "金", "土" ];
+            var today = new Date();
+            console.log(today.getDay())
+            var out = "今日は "+ (today.getMonth() + 1) + " 月の\n";
+            out += "第" + Math.floor((today.getDate() + 6 ) / 7) + weekdays[today.getDay()] + "曜日\n";
+            out += "第" + Math.floor((today.getDate() - today.getDay() + 12 ) / 7) + "週目\n";
+            alert (out);
+        },
+        getCountData(val){
+            var count = []
+            for(const elem of val){
+                count.push(elem.count)
+            }
+            return count
+        },
+        setData(totalval, topicval){
+            this.graphDataTotal = this.getCountData(totalval)
+            this.graphDataTopic = this.getCountData(topicval)
+        },
+      fillData () {
+        this.datacollection = {
+            labels: this.graphLabel,
+            datasets: [
+                {
+                    label: 'total',
+                    backgroundColor: 'lightgrey',
+                    fill: false,
+                    data: this.graphDataTotal
+                }, 
+                {
+                    label: this.topic,
+                    backgroundColor: 'black',
+                    fill: false,
+                    type: 'bar',
+                    data: this.graphDataTopic
+                }
+            ],
+            options: {
+                scales: {
+                xAxes: [{
+                    scaleLabel: {
+                    display: true,
+                    labelString: this.topic
+                    }
+                }],
+                // yAxes: [{
+                //     ticks: {
+                //     beginAtZero: true,
+                //     stepSize: 10,
+                //     }
+                // }]
+                }
+            }
+        }
+      },
+      pushTopicBtn(val){
+          switch(val){
+                case 'Face Mask':
+                    this.topicMonthData = this.maskMonthData
+                    this.topicDayData = this.maskDayData
+                    console.log('Face Mask')
+                    break;
+                case 'Vaccine':
+                    this.topicMonthData = this.vaccineMonthData
+                    this.topicDayData = this.vaccineDayData
+                    console.log('Vaccine')
+                    break;
+                case 'Yahoo!News':
+                    this.topicMonthData = this.yahooMonthData
+                    this.topicDayData = this.yahooDayData
+                    console.log('Yahoo!News')
+                    break;
+                default:
+                window.alert('error about pushTopicBtn()')
+          }
+          this.topic = val
+          this.pushDateBtn(this.date)
+      },
+      pushDateBtn(val){
+          switch(val){
+            case 'year':
+                this.setData(this.totalMonthData, this.topicMonthData)
+                this.graphLabel = this.monthRange
+                this.dateState = 1
+                this.fillData()
+                break;
+            case 'month':
+                this.graphDataTotal = this.getCountData(this.totalDayData)
+                this.graphDataTopic = this.getCountData(this.topicDayData)
+                this.graphLabel = this.dayRange
+                this.dateState = 2
+                this.fillData()
+                break;
+            // case 'day':
+            //     this.setData(this.totalDayData, this.topicDayData)
+            //     this.graphLabel = this.selectDay
+            //     this.dateState = 3
+            //     break;
+            default:
+                window.alert('error about pushDateBtn()')
+          }
+          this.date = val
+          this.fillData()
+      }
+    },
+    watch: {
+        selectYear:{
+            handler(){
+                switch(this.dataState){
+                    case 1:
+                        if(this.selectYear > 0){
+                            this.pushTopicBtn(this.topic)
+                            break;
+                        } else{
+                            break;
+                        }
+                    case 2:
+                        if(this.selectYear > 0 && this.selectMonth > 0){
+                            this.pushTopicBtn(this.topic)
+                            break;
+                        } else{
+                            break;
+                        }
+                    case 3:
+                        if(this.selectYear > 0 && this.selectMonth > 0 && this.selectDay > 0){
+                            this.pushTopicBtn(this.topic)
+                            break;
+                        } else{
+                            break;
+                        }
+                }
+            }
+        },
+        selectMonth:{
+            handler(){
+                this.dayRange.length = 0
+                this.totalDayData.filter(data => {
+                    if(data.date.substring(4, 6) ==  this.selectMonth && this.dayRange.includes(data.date.substring(6, 8)) == false){
+                        this.dayRange.push(data.date.substring(6, 8))
+                    }
+                })
+                this.fillData()
+            }
+        },
+        selectDay:{
+            handler(){
+                this.fillData()
+            }
+        }
+    }
+  }
+</script>
+
+<style>
+    h1{
+        text-align: center;
+    }
+    .topicBtn{
+        text-align: center;
+    }
+    .dateBtn{
+        text-align: center;
+    }
+    .small {
+        max-width: 640px;
+        margin:  150px auto;
+    }
+</style>
